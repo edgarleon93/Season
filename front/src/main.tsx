8090ffa5ca92e2768b8f1d2f5444a373e523b770
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-03-22 16:06:36 +0100

    First page done

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -10,6 +10,7 @@ import Button from './components/Buttons/Button';
 import InviteButton from './components/Buttons/InviteButton';
 import ButtonComponent from './components/Buttons/IconButton';
 import IconBUtton from './components/Buttons/IconButton';
+import FirstPage from './page/firstPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           element={<InviteButton onClick={() => console.log('yes')}></InviteButton>}
         />
         <Route path="/Heart" element={<IconBUtton type="heart"></IconBUtton>} />
+        <Route path="/FirstPAge" element={<FirstPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
