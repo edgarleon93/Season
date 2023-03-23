6fe392a312b8b2604e4ea2f722281ede0d4ca66a
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-03-23 10:24:43 +0100

    add responsive first page

--- front/src/page/firstPage.tsx
+++ front/src/page/firstPage.tsx
@@ -4,27 +4,37 @@ import Button from '~/components/Buttons/Button';
 function FirstPage() {
   return (
     <>
-      <div className="flex">
-        <div className="z-5 mt-28 flex justify-center">
-          <h1 className="text-red text-8xl">Season</h1>
+      <div className="sm:flex">
+        <div className="">
+          <img
+            className="hidden h-screen w-[40vw] sm:block"
+            src="./img/BgDesk.png"
+            alt=""
+          />
         </div>
-        <img className="sm:hidden" src="./img/PPhone.png" alt="" />
-        <h2 className=" text-center text-3xl tracking-widest text-white	">
-          Add Flavor to your <br /> social media experience.
-        </h2>
-        <h2 className="mt-8 text-center text-3xl tracking-widest text-white	">
-          It's time to spice up <br /> your social life!
-        </h2>
-        <span className="mt-16 flex justify-center">
-          <Button variant="primary" onClick={() => console.log('Button clicked')}>
-            <h2 className="px-12 text-3xl">register now !</h2>
-          </Button>
-        </span>
-        <p className="mt-3 flex justify-center text-white">
-          Already have an account? <a className="text-red ml-2 underline"> Login</a>{' '}
-        </p>
-        <div className="fixed bottom-0 left-0 pb-2 text-white">
-          <a className="px-2 text-[0.8rem]">Cookie policy</a>
+        <div className="sm:w-[60vw]">
+          <div className="mt-28 flex justify-center sm:mt-36 sm:mb-12">
+            <h1 className="text-red text-8xl md:text-9xl">Season</h1>
+          </div>
+          <img className="sm:hidden" src="./img/PPhone.png" alt="" />
+          <h2 className=" text-center text-3xl tracking-widest text-white md:text-4xl	">
+            Add Flavor to your <br /> social media experience.
+          </h2>
+          <h2 className="mt-8 text-center text-3xl tracking-widest text-white md:text-4xl	">
+            It's time to spice up <br /> your social life!
+          </h2>
+          <span className="mt-16 flex justify-center">
+            <Button variant="primary" onClick={() => console.log('Button clicked')}>
+              <h2 className="px-12 text-3xl">register now !</h2>
+            </Button>
+          </span>
+          <p className="mt-3 flex justify-center text-white">
+            Already have an account?{' '}
+            <button className="text-red ml-2 underline"> Login</button>{' '}
+          </p>
+          <div className="fixed bottom-0 left-0 pb-2 text-white">
+            <button className="px-2 text-[0.8rem]">Cookie policy</button>
+          </div>
         </div>
       </div>
     </>
