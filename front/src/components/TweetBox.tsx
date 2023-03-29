8ac094076a16a1daaec60bcce908e3457e53d07d
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-03-29 14:20:51 +0200

    add function to the send button to console log the message written in the text area need to send the written texte to the post section

--- front/src/components/TweetBox.tsx
+++ front/src/components/TweetBox.tsx
@@ -33,7 +33,7 @@ function TweetBox() {
         {isActive && (
           <button
             onClick={handleButtonClick}
-            className=" absolute bottom-0 right-0 mb-2 mr-2 rounded bg-blue-500 py-2 px-4 text-white"
+            className=" bg-red absolute bottom-0 right-0 mb-2 mr-2 rounded py-2 px-4 text-white"
           >
             Send
           </button>
