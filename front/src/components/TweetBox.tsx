e149260dc52d5cf777b702b7efad9db765af8a0c
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-03-29 09:32:38 +0200

    home page try tweetbox

--- front/src/components/TweetBox.tsx
+++ front/src/components/TweetBox.tsx
@@ -5,28 +5,21 @@ import Button from './Buttons/Button';
 function TweetBox() {
   return (
     <div className="flex">
-      <img
-        className="mt-4 h-14 w-14 rounded-full object-cover"
-        src="img/avatar1.webp"
-        alt=""
-      />
       <div className="mt-4">
+        <img
+          className="mt-4 h-10 w-10 items-center rounded-full object-cover"
+          src="img/avatar1.webp"
+          alt=""
+        />
         <form>
           <input
-            className="bg-backtext w-72 rounded-3xl px-3 py-3.5 text-white outline-0"
+            className="bg-backtext w-full rounded-3xl px-3 py-3.5 text-white outline-0"
             type="textarea"
             placeholder="What's up?"
           />
-          <div>
-            {/* <div>
-              <Image className="text-red" />
-            </div> */}
-            <div className="flex justify-end">
-              <Button variant="tertiary">Send</Button>
-            </div>
-          </div>
         </form>
       </div>
+      <Button variant="tertiary">Post</Button>
     </div>
   );
 }
--- front/src/page/HomePage.tsx
+++ front/src/page/HomePage.tsx
@@ -1,12 +1,25 @@
 import axios from 'axios';
 import { Navbar } from '~/components/Navbar';
 import Publication from '~/components/Publication';
+import TweetBox from '~/components/TweetBox';
 
 function HomePage() {
   return (
     <>
       <Navbar />
-      <div className=" mt-4 border-t-2 border-b-2 border-white pt-2 pb-2"> hello</div>
+      <div className=" mt-2 flex place-content-around border-t border-b border-white pt-1 pb-1">
+        <button className=" rounded-full px-4 text-2xl text-white">
+          {' '}
+          <h2>FEED</h2>{' '}
+        </button>
+        <button className="text-gray rounded-full px-4 text-2xl">
+          {' '}
+          <h2>TREND</h2>{' '}
+        </button>
+      </div>
+      <div>
+        <TweetBox />
+      </div>
       <Publication />
     </>
   );
