ae63652deac14b1f96798b21e15549b355ebe305
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-04-18 13:57:01 +0200

    conected to back and add fake date

--- front/src/components/Publication.tsx
+++ front/src/components/Publication.tsx
@@ -1,65 +1,100 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import IconButton from './Buttons/IconButton';
 import axios from 'axios';
+import { usePosts } from '../contexts/PostContext';
+
+async function fetchAllUsers() {
+  try {
+    const response = await axios.get('https://season-app-hbxam.ondigitalocean.app/all');
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch user data:', error);
+    return null;
+  }
+}
 
 function Publication() {
-  interface User {
-    _id: number;
-    userId: any;
-    text: string;
-    likes: string[];
-    comments: string[];
-  }
+  const { posts, setPosts } = usePosts();
 
-  interface PostsResponse {
-    posts: Post[];
-  }
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await axios.get(
+          'https://season-app-hbxam.ondigitalocean.app/all/posts',
+        );
+        const fetchedPosts = response.data.posts;
 
-  axios
-    .get<PostsResponse>('https://season-app-hbxam.ondigitalocean.app/all/posts')
-    .then((response) => {
-      const posts = response.data.posts;
-      const texts = posts.map((post) => post.text);
-      // console.log(texts);
-      console.log(posts);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+        // Trier les posts par date
+        fetchedPosts.sort(
+          (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+        );
+
+        const allUsers = await fetchAllUsers();
+        const usersById = {};
+        if (allUsers) {
+          allUsers.forEach((user) => {
+            usersById[user._id] = user;
+          });
+        }
+
+        const postsWithUserData = fetchedPosts.map((post) => {
+          const userData = usersById[post.userId._id];
+          if (userData) {
+            post.userData = userData;
+          }
+          return post;
+        });
+
+        if (Array.isArray(postsWithUserData)) {
+          setPosts(postsWithUserData);
+        } else {
+          console.error('API response is not an array');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchPosts();
+  }, [setPosts]);
 
   return (
     <>
-      <div className="flex  p-4">
-        <img className="mr-4 h-12 w-12 rounded-full" src="" alt="Avatar" />
-        <div className="flex-1">
-          <div className="mb-2 flex items-center">
-            <p className="mr-2 text-xl font-extrabold text-white">John Doe</p>
-          </div>
-          <p className="mb-4 text-white">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum nunc et
-            orci tristique, at bibendum magna cursus. Nulla porttitor massa vel ultrices
-            vestibulum. Suspendisse commodo metus vel ex pellentesque ullamcorper.
-          </p>
-          <div className="flex items-center justify-end">
-            <button className=" text-white hover:text-white">
-              <IconButton type="heart" onClick={() => console.log('heart clicked')} />
-
-              <span className="ml-1">10</span>
-            </button>
-            <button className="mx-4 text-white hover:text-white">
-              <IconButton
-                type="messageSquare"
-                onClick={() => console.log('messageSquare clicked')}
-              />
-
-              <span className="ml-1">5</span>
-            </button>
+      {posts.map((post) => (
+        <div className="mx-2 flex border-b border-white p-2 pt-4" key={post._id}>
+          <img
+            className="mr-4 h-12 w-12 rounded-full"
+            src={post.userData.profilePic || ''}
+            alt="Avatar"
+          />
+          <div className="flex-1">
+            <div className="mb-2 flex items-center">
+              <p className="mr-2 text-xl font-extrabold text-white">
+                {post.userData.username || 'John Doe'}
+              </p>
+            </div>
+            <p className="mb-4 text-white">{post.text}</p>
+            <div>
+              <div className="flex items-center justify-end">
+                <button className=" text-white hover:text-white">
+                  <IconButton type="heart" onClick={() => console.log('heart clicked')} />
+                  <span className="ml-1">{post.likes.length}</span>
+                </button>
+                <button className="mx-2 text-white hover:text-white">
+                  <IconButton
+                    type="messageSquare"
+                    onClick={() => console.log('messageSquare clicked')}
+                  />
+                  <span className="ml-1">{post.comments.length}</span>
+                </button>
+              </div>
+              {/* <div className="flex justify-center">
+                <span className="mt-4 block w-11/12 border-t border-white"></span>
+              </div> */}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <span className="block w-11/12 border-t border-white"></span>
-      </div>
+      ))}
     </>
   );
 }
