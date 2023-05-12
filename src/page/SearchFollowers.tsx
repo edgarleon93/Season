a406c8f410281fc5651ce7b95e04f85db0749c04
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-05-12 10:14:23 +0200

    test deploy

--- src/page/SearchFollowers.tsx
+++ src/page/SearchFollowers.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar } from '~/components/Navbar';
-import NavigateBar from '~/components/navigateBar';
+import NavigateBar from '~/components/NavigateBar';
 
 import { SearchBar } from '~/components/search/SearchBar';
 
